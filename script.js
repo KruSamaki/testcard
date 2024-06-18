916b914400475701bb7cb613abdcc3b804691ad9
KruSamaki <129198342+KruSamaki@users.noreply.github.com>
2024-06-18 11:38:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Click show or hide
+function toggleMobileMenu() {
+  var mobileMenu = document.getElementById("ul");
+  if (mobileMenu.style.display === "block") {
+    mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "block";
+  }
+}
